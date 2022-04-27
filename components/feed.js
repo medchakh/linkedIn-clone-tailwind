3dd3f7d8afad6c3e919dbaffa4e03e43add92cb2
medchakhman <medchakhman@gmail.com>
2022-04-27 17:41:25 +0000

    updated and fixed exports issuest 2

--- components/feed.js
+++ components/feed.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { useRecoilState } from "recoil"
-import Input from "./Input"
+import Input from "./input"
 import { handlePostState, useSSRPostsState } from "../atoms/postAtom"
 import Post from "./Post"
 
